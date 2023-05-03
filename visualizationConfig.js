757d5f92288060cffeafda1b3a24fca42a0d0f88
Dan Melinger <dan@melinger.net>
2023-05-03 12:30:04 -0400

    Create visualizationConfig.js
    Config file to list visualizations.

---
+++ visualizationConfig.js
@@ -0,0 +1,12 @@
+export const visualizations = [
+  {
+    name: "Frequency Bars",
+    file: "./visualizations/frequencyBars.js",
+    function: "drawFrequencyBars",
+  },
+  {
+    name: "Radial",
+    file: "./visualizations/radial.js",
+    function: "drawRadial",
+  },
+];
